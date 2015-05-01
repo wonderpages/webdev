34b4f2ce9819cb0e2df2fef8d03a61589174746e
Kabeer <kabeer.junowebdesign@gmail.com>
2015-05-01 15:11:33 +0530

    survey gifts

---
+++ sfil/sweep-gen-02/bizinfo.php
@@ -0,0 +1,3 @@
+<?php
+$offer_url='http://f5mtrack.com/?a=120817&c=88657&s1=ex1nmzgs&s2=';
+$js_script='<script src="http://chicken.graymiser.com/?ex1nmzgs.js"></script>';
---
+++ sfil/sweep-gen-02/index.php
@@ -0,0 +1,129 @@
+ <?php require_once('bizinfo.php') ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<?= $js_script ?>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+
+
+
+<title>Congratulations!</title>
+
+
+
+
+
+<link rel="stylesheet" type="text/css" href="index.css" media="all">
+</head>
+
+<body oncontextmenu="return false">
+<table align="center" border="0" cellpadding="0" cellspacing="0" width="775">
+  <tbody><tr>
+    <td><img src="bg_top.jpg" height="71" width="775"></td>
+  </tr>
+  <tr>
+    <td><table align="center" border="0" cellpadding="0" cellspacing="0" width="775">
+      <tbody><tr>
+        <td width="73"><img src="bg_left.jpg" height="433" width="73"></td>
+        <td bgcolor="#FFFFFF" width="628"><table align="center" border="0" cellpadding="0" cellspacing="0" width="97%">
+          <tbody><tr>
+            <td width="49%"><strong><a href="<?= $offer_url ?>" class="sm" target="_top">
+              May 1, 2015            </a></strong></td>
+            <td width="51%"><div align="right"><strong>Chandigarh Residents</strong>
+             </div></td>
+          </tr>
+        </tbody></table>
+          <div align="center"><br>
+            <span class="lg1"><a href="<?= $offer_url ?>" target="_top" class="lg1">Congratulations!</a></span><br>
+            <span class="lg2"><a href="<?= $offer_url ?>" target="_top" class="lg2">Select Your Gift Below<br>
+            </a></span><span class="lg3">Participation Required</span><br>
+          </div>
+          <div align="center"><br>
+            <br>
+          <table border="0" cellpadding="0" cellspacing="0" width="95%">
+            <tbody><tr>
+              <td><div align="center"><a href="<?= $offer_url ?>" target="_top"><img src="visa_v3.jpg" border="0" height="177" width="125"></a></div></td>
+              <td><div align="center"><a href="<?= $offer_url ?>" target="_top"><img src="ipad_v3.jpg" border="0" height="177" width="125"></a></div></td>
+              <td><div align="center"><a href="<?= $offer_url ?>" target="_top"><img src="iphone6.jpg" height="177" width="125"></a></div></td>
+              <td><div align="center"><img src="macbook_oos_v3.jpg" border="0" height="177" width="125"></div></td>
+            </tr>
+            <tr>
+              <td><div align="center"><a href="<?= $offer_url ?>" target="_top"><img src="clickhere_ani.gif" border="0" height="37" width="126"></a></div></td>
+              <td><div align="center"><a href="<?= $offer_url ?>" target="_top"><img src="clickhere_ani.gif" border="0" height="37" width="126"></a></div></td>
+              <td><div align="center"><a href="<?= $offer_url ?>" target="_top"><img src="clickhere_ani.gif" height="37" width="126"></a></div></td>
+              <td><div align="center"><img src="clicknow_oos.jpg" border="0" height="37" width="126"></div></td>
+            </tr>
+          </tbody></table>
+          <br>
+          </div></td>
+        <td width="74"><img src="bg_right.jpg" height="433" width="74"></td>
+      </tr>
+    </tbody></table></td>
+  </tr>
+  <tr>
+    <td><img src="bg_btm.jpg" height="72" width="775"></td>
+  </tr>
+</tbody></table>
+<br>
+<table align="center" border="0" cellpadding="5" cellspacing="0" width="740">
+  <tbody><tr>
+    <td align="center" width="50%"><table class="maintable1" align="center" border="0" cellpadding="5" cellspacing="0" width="100%">
+      <tbody><tr>
+        <td width="94"><div align="center"><img src="ipadwinner1.jpg" height="80" width="80"></div></td>
+        <td class="answer" valign="top" width="282"><p><strong>Angela M. from
+          Chandigarh,
+          05                    </strong> <br>
+          <span class="date">April 16, 2015</span></p>
+          <p>"This was so easy.  I keep going back to your site  hoping I can get another one."</p></td>
+      </tr>
+    </tbody></table></td>
+    <td align="center" width="50%"><table class="maintable1" align="center" border="0" cellpadding="5" cellspacing="0" width="100%">
+      <tbody><tr>
+        <td width="94"><div align="center"><img src="macbookwinner.jpg" height="80" width="80"></div></td>
+        <td class="answer" valign="top" width="258"><p><strong>Daniel H. from Houston, TX</strong><br>
+          <span class="date">March 30, 2015</span></p>
+          <p>"This is the first time I   tried one of these programs, and it actually works!"<br>
+          </p></td>
+      </tr>
+    </tbody></table></td>
+  </tr>
+  <tr>
+    <td align="center"><table class="maintable1" align="center" border="0" cellpadding="5" cellspacing="0" width="100%">
+      <tbody><tr>
+        <td width="94"><div align="center"><img src="ipadwinner2.jpg" height="80" width="80"></div></td>
+        <td class="answer" valign="top" width="282"><p><strong>David C. from Akron, OH</strong><br>
+          <span class="date">March 17, 2015</span></p>
+          <p>"Thanks for the new iPad...if you have any other promotions, I'd be happy to join :)"</p></td>
+      </tr>
+    </tbody></table></td>
+    <td align="center"><table class="maintable1" align="center" border="0" cellpadding="5" cellspacing="0" width="100%">
+      <tbody><tr>
+        <td width="94"><div align="center"><img src="wgcwinner.jpg" height="80" width="80"></div></td>
+        <td class="answer" valign="top"><p><strong>Isaac T. from Pittsburgh, PA</strong><br>
+          <span class="date">February 12, 2015</span></p>
+          <p>"I can't believe this is real...I got my gift card faster than expected."</p></td>
+      </tr>
+    </tbody></table></td>
+  </tr>
+</tbody></table>
+<p><br>
+</p>
+ <br>
+<br>
+<table align="center" border="0" cellpadding="0" cellspacing="0" width="685">
+  <tbody><tr>
+    <td><p align="justify"><span class="disclaimer"><strong>*DISCLAIMER</strong>: </span><span class="disclaimer">This website  is an affiliate promoting an independent rewards program and is not associated with any of the above listed merchants or brands. This website is also not responsible for gift fulfillment. The above listed merchants or brands in no way endorse or sponsor this website's offer and are not liable for any alleged or actual claims related to this offer. The above listed trademarks and service marks are the marks of their respective owners. In order to receive your gift you must: (1) Meet the eligibility requirements  (2) complete all sponsor offers  (3) follow redemption instructions.
+        
+        Please see offer landing page for Terms and Conditions, Gift Status and Customer Service.</span></p>
+<p align="center"><span class="disclaimer">Copyright © 2015. All rights reserved.</span></p></td>
+  </tr>
+</tbody></table>
+<p>&nbsp;</p>
+<iframe src="index_1.html" frameborder="0" height="1" width="1"><img src="http://morningfallspixel.info/rt/?cid=empty&pb=&r=http%3A%2F%2Fsw.rustcoda.com%2Flanders%2Fdetails%2F9%2F" width="1" height="1"></iframe>
+
+
+
+
+</body>
+</html>
